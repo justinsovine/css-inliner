cd8b248424b67c8e10756b20f105c155e02bbe1a
Justin Sovine <justinsovine@gmail.com>
2015-04-01 23:20:11 -0400

    Basic inlining working

---
+++ css-inliner.py
@@ -0,0 +1,34 @@
+#!/bin/env python
+
+# # # # # # # # # # # # #
+# Title: CSS Inliner
+# Author: Justin Sovine
+# Date: April 01, 2015
+# # # # # # # # # # # # #
+
+import json
+import mailchimp
+
+def get_mailchimp_api():
+    return mailchimp.Mailchimp('4ef9f73e3b3d1d5a7b2a71c1daf6ebf3-us8') #your api key here
+
+def inline_css(html):
+    try:
+        m = get_mailchimp_api()
+        return m.helper.inline_css(html)
+    except mailchimp.Error, e:
+        messages.error(request,  'An error occurred: %s - %s' % (e.__class__, e))
+
+def main():
+    with open ("example.html", "r") as html:
+        data=html.read()
+
+    #print data
+    html = str(data)
+    inlinedHtml = inline_css(html)
+    #print json.dumps(inlinedHtml, sort_keys=True, indent=4, separators=(',', ': '))
+    #print inlinedHtml
+    print '%s' % inlinedHtml['html']
+
+if __name__ == "__main__":
+    main()
